3692d4c03325873e270d74d3292037618f5b509a
Eric Fiorani <efiorani305@gmail.com>
2018-10-24 18:05:14 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,26 +11,21 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
-	def edit
-	  @post = Post.find(params[:id])
+	def create
+	  @post = Post.new(post_params)
+	  @post.save
+	  redirect_to post_path(@post)
 	end
-
-	
-def create
-  @post = Post.new(post_params)
-  @post.save
-  redirect_to post_path(@post)
-end
- 
-def update
-  @post = Post.find(params[:id])
-  @post.update(post_params)
-  redirect_to post_path(@post)
-end
- 
-private
- 
-def post_params
-  params.require(:post).permit(:title, :description)
-end
+	 
+	def update
+	  @post = Post.find(params[:id])
+	  @post.update(post_params)
+	  redirect_to post_path(@post)
+	end
+	 
+	private
+	 
+	def post_params
+	  params.require(:post).permit(:title, :description)
+	end
 end
